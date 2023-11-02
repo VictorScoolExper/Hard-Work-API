6a94dc276cd56919487ed8d18b4eb5a3afa03782
VictorScoolExper <victorscool477@outlook.com>
2023-11-01 20:37:50 -0600

    modified s3.test

--- test/src/utils/aws/s3.test.js
+++ test/src/utils/aws/s3.test.js
@@ -1,88 +1,88 @@
 /* Green Work ERP by Victor Martinez */
 
-import { vi, test, describe, it, expect, beforeAll, beforeEach } from 'vitest';
+import { vi, test, describe, it, expect, beforeAll, beforeEach } from "vitest";
 
 import {
   addObjectS3Bucket,
   createSignedUrls,
   updateS3ObjectBucket,
   deleteObjectS3Bucket,
-} from '../../../../src/utils/aws/s3.js';
+} from "../../../../src/utils/aws/s3.js";
 
-// Mocks
-vi.mock('@aws-sdk/client-s3', () => {
-  const S3Client = vi.fn(() => ({
-    send: vi.fn(() => {
-      return;
-    }),
-  }));
-  const PutObjectCommand = vi.fn();
-  const GetObjectCommand = vi.fn();
-  const DeleteObjectCommand = vi.fn();
-
-  return {
-    S3Client,
-    PutObjectCommand,
-    GetObjectCommand,
-    DeleteObjectCommand,
-  };
-});
-vi.mock('@aws-sdk/s3-request-presigner', () => {
-  const getSignedUrl = vi.fn();
-
-  return {
-    getSignedUrl,
-  };
-});
-vi.mock('crypto', () => {
-  return {
-    default: {
-      randomBytes: vi.fn(() => {
-        return 'name-crypto-random-data';
+describe("s3", () => {
+  // Mocks
+  vi.mock("@aws-sdk/client-s3", () => {
+    const S3Client = vi.fn(() => ({
+      send: vi.fn(() => {
+        return;
       }),
-    },
-  };
-});
+    }));
+    const PutObjectCommand = vi.fn();
+    const GetObjectCommand = vi.fn();
+    const DeleteObjectCommand = vi.fn();
 
-vi.mock('../../configs/config.js', () => {
-  return {
-    aws: {
-      bucketName: 'testBucketName',
-      bucketRegion: 'testBucketRegion',
-      accessKey: 'testAccesKey',
-      secretAccessKey: 'secretAccessKey',
-    },
-  };
-});
+    return {
+      S3Client,
+      PutObjectCommand,
+      GetObjectCommand,
+      DeleteObjectCommand,
+    };
+  });
+  vi.mock("@aws-sdk/s3-request-presigner", () => {
+    const getSignedUrl = vi.fn();
 
-// Spies
-const expected = 'name-crypto-random-data';
-// vi.spyOn(crypto, 'randomBytes').mockReturnValue(expected);
+    return {
+      getSignedUrl,
+    };
+  });
+  vi.mock("crypto", () => {
+    return {
+      default: {
+        randomBytes: vi.fn(() => {
+          return "name-crypto-random-data";
+        }),
+      },
+    };
+  });
 
-describe('s3', () => {
-  describe('addObjectS3Bucket', () => {
-    const buffer = Buffer.from('image_data');
+  vi.mock("../../configs/config.js", () => {
+    return {
+      aws: {
+        bucketName: "testBucketName",
+        bucketRegion: "testBucketRegion",
+        accessKey: "testAccesKey",
+        secretAccessKey: "secretAccessKey",
+      },
+    };
+  });
+
+  // Spies
+  const expected = "name-crypto-random-data";
+  // vi.spyOn(crypto, 'randomBytes').mockReturnValue(expected);
+
+  describe("addObjectS3Bucket", () => {
+    const buffer = Buffer.from("image_data");
     const file = {
-      mimetype: 'image/jpeg',
+      mimetype: "image/jpeg",
     };
 
     beforeEach(() => {
       vi.clearAllMocks();
     });
 
-    it('should add an object to s3', async () => {
+    it("should add an object to s3", async () => {
       const imageName = await addObjectS3Bucket(buffer, file);
 
       expect(imageName).toEqual(expected);
     });
 
-    it('should execute the crypto.randomBytes', async () => {
+    it("should execute the crypto.randomBytes", async () => {
       await addObjectS3Bucket(buffer, file);
 
       expect(crypto.randomBytes).toBeCalled();
     });
 
-    it('should execute PutObjectCommand method', async () => {
+    it("should execute PutObjectCommand method", async () => {
       await addObjectS3Bucket(buffer, file);
 
       expect(PutObjectCommand).toBeCalled();
