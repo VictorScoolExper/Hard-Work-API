3082b7537a3d8db543934370fd144187bd3f2c5a
VictorScoolExper <victorscool477@outlook.com>
2023-11-22 16:11:39 -0600

    added user.js

--- test/src/models/user.test.js
+++ test/src/models/user.test.js
@@ -1,23 +1,54 @@
-// /* Green Work ERP by Victor Martinez */
+/* Green Work ERP by Victor Martinez */
 
-// import { vi, describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, beforeEach } from "vitest";
 
-// import User from '../../../src/models/user';
+import User from "../../../src/models/user";
+import { connection } from "../../../src/utils";
 
+describe("user class", () => {
+  vi.mock("../../../src/models/user");
+  vi.mock("../../../src/utils");
 
-// describe("user class", () => {
-//     vi.mock('connection', () => {
-//         return {
-//             query: (sql, params, callback) => {
-//                 return 'success';
-//             }
-//         }
-//     });
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-//     it('should retrieve user information from database', async () => {
-//         const userEmail = 'test@example.com';
-//         const userInfo = await User.getUserInfo(userEmail);
+  it("should execute the method getUserInfo", async () => {
+    const userEmail = "test@example.com";
 
-//         expect(userInfo).toBeTruthy();
-//     })
-// })
+    await User.getUserInfo(userEmail);
+
+    expect(User.getUserInfo).toBeCalled();
+  });
+
+  it('should execute the method findEmail Auth', async () => {
+    const userEmail = "test@example.com";
+
+    await User.findEmailAuth(userEmail);
+
+    expect(User.findEmailAuth).toBeCalled();
+  });
+
+  it('should execute the method isUserEmpty', async () => {
+    await User.isUserEmpty();
+
+    expect(User.isUserEmpty).toBeCalled();
+  });
+
+  it('should execute the method createUser', async () => {
+    const user = {
+        name: 'test', 
+        last_name: 'testLastName',
+        cell_number: '+524771234567',
+        role: 'admin',
+        birth_date: Date.now,
+        email: 'example@email.com',
+        password: '123ABCabc'
+    }
+    
+    await User.createUser(user);
+
+    expect(User.createUser).toBeCalled();
+  });
+
+});
